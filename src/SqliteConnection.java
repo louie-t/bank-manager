73755d8943313030898ce887a0149fb779e393ff
louie.tang <40411250+louietang@users.noreply.github.com>
2019-10-14 12:51:24 -0600

    first commit

---
+++ src/SqliteConnection.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+import javax.swing.*;
+
+public class SqliteConnection {
+	Connection dbConnection  = null;
+	
+	public static Connection dbConnector() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			String path = "UserData.sqlite";
+			Connection dbConnection = DriverManager.getConnection("jdbc:sqlite:" + path);
+			JOptionPane.showMessageDialog(null,  "Connection Successful");
+			return dbConnection;
+		}catch(Exception e) {
+			JOptionPane.showMessageDialog(null,  e);
+			return null;
+		}
+	}
+}
